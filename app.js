83b5e273b00348ef4ef61530724519272da73cb1
Niraj <ndedhia@amazon.com>
2023-03-08 15:36:56 -0800

    Modified somde npm packages

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/sample", (req, res, next) => {
- res.json(["Niraj", "Vaibhav"]);
+ res.json(["Prod1", "Prod2"]);
 });
 
 app.listen(3000, () => {
