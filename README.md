b19bdc790f67363906221b246c10a3f0680839ad
Niraj <ndedhia@amazon.com>
2023-03-08 15:45:38 -0800

    Added ReadMe and some test services

---
+++ README.md
@@ -0,0 +1,18 @@
+## Small business
+
+### How to push
+- git add .
+- git commit -m "<Add your message>"
+- git push -u origin main
+
+### Run locally 
+- node app.js
+- Visit http://localhost:3000/sample
+
+### How to deploy?
+- Just commit your changes and push it to Master it will automatically get deploy
+
+### Post deployment
+- Verify changes using live App link
+- https://ill-gold-parrot-sock.cyclic.app 
+
--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 var express = require("express");
 var app = express();
 
-app.get("/sample", (req, res, next) => {
- res.json(["Prod1", "Prod2"]);
+app.get("/", (req, res, next) => {
+    res.json("Welcome to our web-service environment!");
 });
+
+app.get("/test", (req, res, next) => {
+ res.json(["Test1", "TEst2"]);
+});
+
+app.get("/products", (req, res, next) => {
+    res.json(["Prod1", "Prod2"]);
+   });
 
 app.listen(3000, () => {
  console.log("Server running on port 3000");
